51b331563abe87050503d5c094aa88828726ec85
Leon-kings <leonakingeneye2002@gmail.com>
2026-04-22 18:35:47 +0200

    changes on menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Navbar } from "./components/navbar/Navbar";
-import { Hero } from "./components/hero/Hero";
-import { About } from "./pages/about/About";
-import { Services } from "./pages/services/Services";
 import { Menu } from "./components/menu/Menu";
 
 export default function App() {
